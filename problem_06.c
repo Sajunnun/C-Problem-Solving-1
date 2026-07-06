6c7991b30fd6a0890db1373ee58bc26050cb4f2e
Sajunnun <sajunnun@gmail.com>
2026-07-06 23:28:23 +0600

    temp celsius to fahr

---
+++ problem_06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int f,c;
+  printf("enter the temp in celsius: \n");
+  scanf("%d",&c);
+  f=(c*9/5)+32;
+  printf("the temp in fahrenheit is: %d \n",f);
+
+  
+
+
+    return 0;
+}
